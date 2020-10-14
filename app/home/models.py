2db249d3e0e975d8e7f3f4d39d8762e5d52e701f
sridhar <sridharjena97@gmail.com>
2020-10-14 12:55:44 +0530

    contact page handled in views, some name chanded in contact model and updated installed_apps in settings

--- app/home/models.py
+++ app/home/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class Contacts(models.Model):
+class Contact(models.Model):
     srl= models.AutoField(primary_key=True)
     name= models.CharField(max_length=150, default='')
-    email= models.EmailField(max_length=254)
-    mobileNumber= models.IntegerField(blank=True, null= True)
-    mesaageTitle= models.CharField(max_length=500)
-    messageBody= models.CharField(max_length=1000)
+    email= models.EmailField(max_length=254, default='')
+    mobileNumber= models.CharField(max_length=13, null=True, default='')
+    messageTitle= models.CharField(max_length=500, default='NA')
+    messageBody= models.CharField(max_length=1000, default='NA')
+    def __str__(self):
+        return self.messageTitle
     
 
 
--- app/home/views.py
+++ app/home/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render,HttpResponse
+from home.models import Contact
 
 # Create your views here.
 def home(request):
     return render(request, 'home/index.html',{'endp':'home'})
 def contact(request):
+    if request.method=='POST':
+        name= request.POST.get('name','')
+        email= request.POST.get('email','')
+        mobile= request.POST.get('mobile','')
+        title= request.POST.get('title','')
+        message= request.POST.get('message','')
+        newQuery= Contact(name=name, email=email, mobileNumber=mobile, messageTitle=title, messageBody=message)
+        newQuery.save()
+        return render(request, 'home/contact.html', {'endp':'contact','deliverd':True})
     return render(request, 'home/contact.html', {'endp':'contact'})
 def about(request):
     return render(request, 'home/about.html', {'endp':'about'})
